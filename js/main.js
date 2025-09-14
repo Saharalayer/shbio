9e8118d93db56c432e6b0383bc6c53cb002b4064
Saharalayer <saharalayer@outlook.com>
2025-09-14 20:56:00 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  toggleBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
   document.querySelector(".cta").addEventListener("click", () => {
     alert("🎉 مرحبًا بك في Tarwij! أنشئ صفحتك الذكية الآن.");
   });
